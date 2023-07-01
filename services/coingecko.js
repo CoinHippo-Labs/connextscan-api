319dfdd35ba6279f00ab6ad122300fe77e3cfae3
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2023-07-01 14:12:33 +0700

    feat: restructure

--- services/coingecko.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-module.exports = async (path = '', params = {}) => {
-  const api = axios.create({ baseURL: 'https://api.coingecko.com/api/v3/', timeout: 20000 });
-  const response = await api.get(path, { params }).catch(error => { return { error: error?.response?.data }; });
-  return response?.data;
-};
